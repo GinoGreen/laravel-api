9e8e1b75d2c1a216bc67fd4371d2693adcf05688
GinoGreen <luigiverde97@gmail.com>
2022-02-09 16:06:48 +0100

    header

---
+++ resources/js/components/partials/Header.vue
@@ -0,0 +1,31 @@
+<template>
+   <header>
+      <ul>
+         <li><a href="">Home</a></li>
+         <li><a href="">Post</a></li>
+         <li><a href="">Chi Siamo</a></li>
+         <li><a href="">Contatti</a></li>
+      </ul>
+   </header>
+</template>
+
+<script>
+export default {
+   name: 'Header'
+}
+</script>
+
+<style lang="scss">
+
+   header {
+      ul {
+         @include reset-list();
+         li {
+
+            a {
+               @include reset-a();
+            }
+         }
+      }
+   }
+</style>
